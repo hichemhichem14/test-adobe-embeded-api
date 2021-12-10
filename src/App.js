3b7449339cf80ee91450ea821841524326608f2a
hichem benchikh <hichemabderrahmene31@gmail.com>
2021-12-10 14:48:15 +0100

    test adobe embeded api and add custom buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import ViewSDKClient from './components/ViewSDKClient';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor( props ){
+      super( props );
+      this.gotopage10 = this.gotopage10.bind(this);
+    }
+     gotopage10=(page)=>{
+      //console.log(page)    
+      //const viewSDKClient = new ViewSDKClient();
+     if(this.viewSDKClient)
+      this.viewSDKClient.gotopage(this.previewfilepromise,page)
+  }
+  componentDidMount() {
+      const viewSDKClient = new ViewSDKClient();
+      this.viewSDKClient=viewSDKClient;
+    viewSDKClient.ready().then(() => {
+          /* Invoke file preview */
+          /* By default the embed mode will be Full Window */
+         this.previewfilepromise=viewSDKClient.previewFile("pdf-div", {},this.props.url);
+          
+      });
+     
+  }
+
+  render() {
+      return (
+  
+      <div className="container">
+        
+         <div  className="layout"> <ul>
+          <li><button onClick={(event)=>this.gotopage10(5)}>go to page number 5</button></li>
+          <li><button onClick={(event)=>this.gotopage10(200)}>go to page number 200</button></li>
+        </ul></div>
+         
+       <div id="pdf-div" className="full-window-div"/></div> );
+  }
 }
-
 export default App;
---
+++ src/components/ViewSDKClient.js
@@ -0,0 +1,130 @@
+class ViewSDKClient {
+    constructor() {
+    this.readyPromise = new Promise((resolve) => {
+    if (window.AdobeDC) {
+    resolve();
+    } else {
+    document.addEventListener("adobe_dc_view_sdk.ready", () => {
+    resolve();
+    });
+    }
+    });
+    this.adobeDCView = undefined;
+    }
+    ready() {
+    return this.readyPromise;
+    }
+    previewFile(divId, viewerConfig, url) {
+    const config = {
+    clientId: "fc9d0f9671774fb2a0f500dd5f4d0f73" //Enter your Client ID here
+    
+    //clientId: "8c0cd670273d451cbc9b351b11d22318" //Enter your Client ID here
+    };
+    if (divId) {
+    config.divId = divId;
+    }
+    this.adobeDCView = new window.AdobeDC.View(config);
+    const previewFilePromise = this.adobeDCView.previewFile(
+    {
+    content: {
+    location: {
+    url: url
+    }
+    },
+    metaData: {
+    fileName: "Menu.pdf",
+    id: "6d07d124-ac85–43b3-a867–36930f502ac6"
+    }
+    },
+    viewerConfig
+    );
+    return previewFilePromise;
+    }
+    previewFileUsingFilePromise(divId, filePromise, fileName) {
+    this.adobeDCView = new window.AdobeDC.View({
+    clientId: "fc9d0f9671774fb2a0f500dd5f4d0f73", //Enter your Client ID here
+    divId
+    });
+    this.adobeDCView.previewFile(
+    {
+    content: {
+    promise: filePromise
+    },
+    metaData: {
+    fileName: fileName
+    }
+    },
+    {}
+    );
+    }
+    registerSaveApiHandler() {
+    const saveApiHandler = (metaData, content, options) => {
+    console.log(metaData, content, options);
+    return new Promise((resolve) => {
+    setTimeout(() => {
+    const response = {
+    code: window.AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
+    data: {
+    metaData: Object.assign(metaData, {
+    updatedAt: new Date().getTime()
+    })
+    }
+    };
+    resolve(response);
+    }, 2000);
+    });
+    };
+    this.adobeDCView.registerCallback(
+    window.AdobeDC.View.Enum.CallbackType.SAVE_API,
+    saveApiHandler,
+    {}
+    );
+    }
+    registerEventsHandler() {
+    this.adobeDCView.registerCallback(
+    window.AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+    (event) => {
+    console.log(event);
+    },
+    {
+    enablePDFAnalytics: true
+    }
+    );
+    }
+    gotopage(previewFilePromise,page){
+        previewFilePromise.then(adobeViewer => {
+
+            adobeViewer.getAPIs().then(apis => {
+                    apis.gotoLocation(page)
+                            .then(() => console.log("Success"))
+                            .catch(error => console.log(error));
+             });
+    });
+    }
+    gotopagev2(previewFilePromise,page){
+        previewFilePromise.then(adobeViewer => {
+
+            adobeViewer.getAPIs().then(apis => {
+                    apis.gotoLocation(page)
+                            .then(() => console.log("Success"))
+                            .catch(error => console.log(error));
+             });
+    });
+    }
+   }
+   export default ViewSDKClient;
+
+
+
+
+/* ////////////////*
+Copyright 2020 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
